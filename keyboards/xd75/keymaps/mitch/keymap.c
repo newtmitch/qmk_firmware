4468dac4daebd656043d24b8f17fb651ae8a28c6
Ryan Mitchell <rmitch@onprem.com>
2017-12-20 23:46:27 -0600

    moved the command key over a tad

--- keyboards/xd75/keymaps/mitch/keymap.c
+++ keyboards/xd75/keymaps/mitch/keymap.c
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC, KC_BSPC },
   { MO(_FN), KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_ENT,  MO(_FN) },
   { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_RSFT, KC_UP,   MO(_SFX)},
-  { KC_LCTL, KC_LALT, KC_LGUI, MO(_FN), KC_SPC, KC_SPC, KC_SPC, KC_SPC, MO(_FN), KC_RGUI, KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT },
+  { MO(_FN), KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_RGUI, KC_RALT, KC_RCTL, MO(_FN), KC_LEFT, KC_DOWN, KC_RGHT },
  },
 
 /* FUNCTION
