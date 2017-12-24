785818886be12518dcb892c52ca944b6bcddbc67
Ryan Mitchell <rmitch@onprem.com>
2017-12-24 14:15:02 -0600

    added mouse support

--- keyboards/dz60/config.h
+++ keyboards/dz60/config.h
@@ -51,4 +51,12 @@
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
 
+/* Mouse settings */
+#define MOUSEKEY_DELAY             150
+#define MOUSEKEY_INTERVAL          50
+#define MOUSEKEY_MAX_SPEED         10
+#define MOUSEKEY_TIME_TO_MAX       20
+#define MOUSEKEY_WHEEL_MAX_SPEED   8
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 40
+
 #endif
